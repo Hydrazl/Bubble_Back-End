c4f3f3a3ba5a08c49a6f5ac93709cc89484b61b2
Hydrazl <joteixeirasimoes@gmail.com>
2025-11-12 22:44:50 -0300

    Tentativa de Backup do Banco de Dados

---
+++ bubble_db_backup.sql
@@ -0,0 +1,174 @@
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bubble_db
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `likes`
+--
+
+DROP TABLE IF EXISTS `likes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `likes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL,
+  `postId` int NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `likes`
+--
+
+LOCK TABLES `likes` WRITE;
+/*!40000 ALTER TABLE `likes` DISABLE KEYS */;
+INSERT INTO `likes` VALUES (7,1,6,'2025-11-03 00:20:10','2025-11-03 00:20:10');
+/*!40000 ALTER TABLE `likes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(255) DEFAULT NULL,
+  `media` varchar(255) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` VALUES (1,'Testando o post','uploads\\1762046434099-416719553.jpg','2025-11-02 01:20:34','2025-11-02 01:20:34'),(2,'Testando o post novamente','uploads\\1762046917324-8970727.jpg','2025-11-02 01:28:37','2025-11-02 01:28:37'),(3,'Testando o post novamente parte 2','uploads/1762048007702-495808514.jpg','2025-11-02 01:46:47','2025-11-02 01:46:47'),(4,'Testando o post novamente parte 3','uploads/1762048087988-318838305.jpg','2025-11-02 01:48:08','2025-11-02 01:48:08'),(5,'Testando o post novamente parte 4',NULL,'2025-11-02 01:50:53','2025-11-02 01:50:53'),(6,'Testando o post novamente parte 4','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVoBDKFmNPFs3KwLZcy0Y6IspYo7zfOr90Ww&s','2025-11-02 01:52:06','2025-11-02 01:52:06');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(25) NOT NULL,
+  `description` varchar(150) DEFAULT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username_2` (`username`),
+  UNIQUE KEY `email_2` (`email`),
+  UNIQUE KEY `username_3` (`username`),
+  UNIQUE KEY `email_3` (`email`),
+  UNIQUE KEY `username_4` (`username`),
+  UNIQUE KEY `email_4` (`email`),
+  UNIQUE KEY `username_5` (`username`),
+  UNIQUE KEY `email_5` (`email`),
+  UNIQUE KEY `username_6` (`username`),
+  UNIQUE KEY `email_6` (`email`),
+  UNIQUE KEY `username_7` (`username`),
+  UNIQUE KEY `email_7` (`email`),
+  UNIQUE KEY `username_8` (`username`),
+  UNIQUE KEY `email_8` (`email`),
+  UNIQUE KEY `username_9` (`username`),
+  UNIQUE KEY `email_9` (`email`),
+  UNIQUE KEY `username_10` (`username`),
+  UNIQUE KEY `email_10` (`email`),
+  UNIQUE KEY `username_11` (`username`),
+  UNIQUE KEY `email_11` (`email`),
+  UNIQUE KEY `username_12` (`username`),
+  UNIQUE KEY `email_12` (`email`),
+  UNIQUE KEY `username_13` (`username`),
+  UNIQUE KEY `email_13` (`email`),
+  UNIQUE KEY `username_14` (`username`),
+  UNIQUE KEY `email_14` (`email`),
+  UNIQUE KEY `username_15` (`username`),
+  UNIQUE KEY `email_15` (`email`),
+  UNIQUE KEY `username_16` (`username`),
+  UNIQUE KEY `email_16` (`email`),
+  UNIQUE KEY `username_17` (`username`),
+  UNIQUE KEY `email_17` (`email`),
+  UNIQUE KEY `username_18` (`username`),
+  UNIQUE KEY `email_18` (`email`),
+  UNIQUE KEY `username_19` (`username`),
+  UNIQUE KEY `email_19` (`email`),
+  UNIQUE KEY `username_20` (`username`),
+  UNIQUE KEY `email_20` (`email`),
+  UNIQUE KEY `username_21` (`username`),
+  UNIQUE KEY `email_21` (`email`),
+  UNIQUE KEY `username_22` (`username`),
+  UNIQUE KEY `email_22` (`email`),
+  UNIQUE KEY `username_23` (`username`),
+  UNIQUE KEY `email_23` (`email`),
+  UNIQUE KEY `username_24` (`username`),
+  UNIQUE KEY `email_24` (`email`),
+  UNIQUE KEY `username_25` (`username`),
+  UNIQUE KEY `email_25` (`email`),
+  UNIQUE KEY `username_26` (`username`),
+  UNIQUE KEY `email_26` (`email`),
+  UNIQUE KEY `username_27` (`username`),
+  UNIQUE KEY `email_27` (`email`),
+  UNIQUE KEY `username_28` (`username`),
+  UNIQUE KEY `email_28` (`email`),
+  UNIQUE KEY `username_29` (`username`),
+  UNIQUE KEY `email_29` (`email`),
+  UNIQUE KEY `username_30` (`username`),
+  UNIQUE KEY `email_30` (`email`),
+  UNIQUE KEY `username_31` (`username`),
+  UNIQUE KEY `email_31` (`email`),
+  UNIQUE KEY `username_32` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Ts-Joao','joteixeirasimoes@gmail.com','0987654321','TS-Jaozih',NULL,'2025-11-01 22:28:16','2025-11-01 22:28:16'),(2,'EUMESMO','exemple.eumesmo@gmail.com','03223019','EUMESMO',NULL,'2025-11-01 23:25:52','2025-11-01 23:25:52');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-11-12 22:34:00
---
+++ db/backup/bubble_backup.sql
@@ -0,0 +1,6 @@
+-- Active: 1762997767624@@127.0.0.1@3306@bubble_db
+CREATE DATABASE bubble_db;
+USE bubble_db;
+SELECT * FROM likes;
+SELECT * FROM posts;
+SELECT * FROM users;

ccc299afa482c22a74c6f18f21dc3c38d0b5ffb4
ktzxs <lucasalvesdacosta070@gmail.com>
2025-11-20 01:50:09 -0300

    um negócio ai 👌

--- src/controllers/post/deletedPostController.js
+++ src/controllers/post/deletedPostController.js
@@ -1,10 +1,11 @@
-import Post from "../../models/postModel.js";
+import Post from '../../models/postModel.js';
 
 export default function deletedPostController(req, res) {
     try {
         const { userID, description, mediaURL } = req.body;
         let media = null;
 
-    if 
+    if {
+        
     }
 }

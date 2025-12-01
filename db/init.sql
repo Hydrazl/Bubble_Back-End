4041cf25b9cdc84c792af98a79f32df23a48ce1a
Henri Baruki <baruki.henri@aluno.ifsp.edu.br>
2025-12-01 15:58:28 -0300

    acaba PLMDS

--- db/init.sql
+++ db/init.sql
@@ -1,4 +1,5 @@
--- Active: 1763217408225@@127.0.0.1@3306@bubble_db
+-- Active: 1756925329403@@127.0.0.1@3306@bubble_db
 DROP DATABASE IF EXISTS bubble_db;
 CREATE DATABASE IF NOT EXISTS bubble_db;
+USE  bubble_db;
 SELECT * FROM users;

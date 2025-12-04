91071e71c6171ad5a610ad24e820a06757f40d3d
Henri Baruki <baruki.henri@aluno.ifsp.edu.br>
2025-12-04 17:22:58 -0300

    esta funcionando

--- src/app.js
+++ src/app.js
@@ -3,35 +3,39 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { fileURLToPath } from 'url';
+
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import likeRoutes from './routes/likeRoutes.js';
-import followRoutes from './routes/followRoutes.js'
+import followRoutes from './routes/followRoutes.js';
+import profileRoutes from './routes/profileRoutes.js'; 
 
 const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.json()); // Nós permite fazer o uso do JSON no corpo de requisições
-app.use(bodyParser.json())
-
+// Middlewares
+app.use(express.json());
+app.use(bodyParser.json());
 
 app.use(cors({
-  origin: "http://localhost:5173", // porta do seu React
+  origin: "http://localhost:5173",
   credentials: true
 }));
 
-app.use("/uploads", express.static("uploads"));
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads'))); // Serve arquivos estáticos
+// Arquivos de upload (IMAGENS)
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 // Rotas
 app.get('/', (req, res) => {
-    res.send('Hello world');
+  res.send('Hello world');
 });
 
 app.use('/', postRoutes);
-app.use('/', followRoutes)
+app.use('/', followRoutes);
 app.use('/', userRoutes);
 app.use('/', likeRoutes);
+app.use('/', profileRoutes); 
+
 
 export default app;

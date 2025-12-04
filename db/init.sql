8a83cfb6018d2d71cf9dd3a5c899a89bc4a9cc67
Henri Baruki <baruki.henri@aluno.ifsp.edu.br>
2025-12-04 15:17:17 -0300

    alteração feita

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,11 @@
--- Active: 1764795308841@@127.0.0.1@3306@bubble_db
+-- Active: 1756925329403@@127.0.0.1@3306@bubble_db
 DROP DATABASE IF EXISTS bubble_db;
 CREATE DATABASE IF NOT EXISTS bubble_db;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'aluno';
+FLUSH PRIVILEGES;
 USE bubble_db;
+
 SELECT * FROM posts;
 SELECT banner FROM users;
 DESCRIBE users;
